53b75d3689a553660271348e1aee1eebd586af88
htunyarzar80 <htunyarzar2001@gmail.com>
2024-08-01 23:11:01 +0630

    [Complete] ui

--- backend-api/routes/stripeRoute.js
+++
@@ -1,7 +0,0 @@
-const { makePayment } = require("../controllers/paymentController");
-
-const router = require("express").Router();
-
-router.post("/payment",makePayment);
-
-module.exports = router;
